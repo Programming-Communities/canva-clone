cb732321acf9c4b624ecc431346d68c78445f6e9
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-04-07 06:47:19 +0500

    Side bar and and sidebar menu and setting up to date

--- app/(routes)/design/_components/SideBar.jsx
+++ app/(routes)/design/_components/SideBar.jsx
@@ -1,18 +1,27 @@
 import { sideBarMenu } from '@/services/Options'
-import React from 'react'
+import React, { useState } from 'react'
+import SideBarSettings from './SideBarSettings';
 
 function SideBar() {
+  const [selectedOption,setSelectedOption]=useState(null);
   return (
+    <div className='flex'>
     <div  className='p-2 w-[120px] border-r h-screen pt-2'>
       {sideBarMenu.map((menu,index)=>(
-    <div key={index} className='p-2 mb-2 flex flex-col 
-    items-center hover:bg-secondary cursoer-pointer    '>
+    <div key={index} className={`p-2 mb-2 flex flex-col 
+    items-center hover:bg-secondary cursor-pointer
+    ${menu.name == selectedOption?.name && 'bg-secondary'}`}
+    onClick={()=> setSelectedOption(menu)}
+    >
+
             <menu.icon />
             <h2 className='mt-1'>{menu.name}</h2>
           
     </div>
         
     ))}
+    </div>
+    <SideBarSettings selectedOption={selectedOption}/>
     </div>
   )
 }
